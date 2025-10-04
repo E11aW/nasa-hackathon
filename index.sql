66b68c2ac5a155c840c020fd0c49edb6e4e5a69e
CharlotteHoyt <charlotte.hoyt@comcast.net>
2025-10-04 13:01:27 -0700

    Fixed spelling.

--- index.sql
+++ index.sql
@@ -2,7 +2,7 @@ select 'shell' as component,
        'custom_theme.css' as css, 
        'custom_theme' as theme,
        'Inter' as font,
-       '[Nasa International Space Apps Challenge 2025](https://github.com/E11aW/nasa-hackathon)' as footer;
+       '[NASA International Space Apps Challenge 2025](https://github.com/E11aW/nasa-hackathon)' as footer;
 
 select
     'html' as component,
