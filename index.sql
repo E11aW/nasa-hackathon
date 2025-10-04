42b5f373ce74058f17d5ae670ed5c0394878efcf
E11aW <Ellaraindeer2022@gmail.com>
2025-10-04 12:30:38 -0700

    increase zoom ability

--- index.sql
+++ index.sql
@@ -1,11 +1,8 @@
 select
     'map' as component,
-    5     as zoom,
-    8     as max_zoom,
     800   as height,
     40   as latitude,
     -110    as longitude,
-    'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png' as tile_source,
     ''    as attribution;
 select
     'rectangle' as icon,
