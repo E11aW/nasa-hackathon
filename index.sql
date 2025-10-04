fbf446f2560a3a43afdbe1f23392c0fa10d9ee64
AbbyHoyt <abby.hoyt@comcast.net>
2025-10-04 13:00:03 -0700

    Fixed HTML/CSS integration and styling.

--- index.sql
+++ index.sql
@@ -1,11 +1,12 @@
-select 
-    'shell'                 as component,
-    'NASA International Space Apps Challenge 2025' as title,
-    'boxed'                 as layout,
-    'en-US'                 as language,
-    'Inter' as font,
-    'custom_theme.css' as css,
-    '[Check Out the GitHub](https://github.com/E11aW/nasa-hackathon)' as footer;
+select 'shell' as component, 
+       'custom_theme.css' as css, 
+       'custom_theme' as theme,
+       'Inter' as font,
+       '[Nasa International Space Apps Challenge 2025](https://github.com/E11aW/nasa-hackathon)' as footer;
+
+select
+    'html' as component,
+        '<h1>Sustainable City Planning</h1>' as html;
 
 SELECT 'map' AS component;
 
