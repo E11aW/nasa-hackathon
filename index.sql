ebeb524316b0f45eb9f277624ce6bdd523e3af69
E11aW <Ellaraindeer2022@gmail.com>
2025-10-04 12:07:36 -0700

    USA

--- index.sql
+++ index.sql
@@ -1,1 +1,12 @@
-SELECT 'map' AS component;
+select
+    'map' as component,
+    5     as zoom,
+    8     as max_zoom,
+    800   as height,
+    40   as latitude,
+    -110    as longitude,
+    'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png' as tile_source,
+    ''    as attribution;
+select
+    'rectangle' as icon,
+    20      as size;
