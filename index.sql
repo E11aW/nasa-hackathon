25abd815a39038e9d4babc9c4625c1dfe7750e36
AbbyHoyt <abby.hoyt@comcast.net>
2025-10-04 15:05:45 -0700

    Started embed styling.

--- index.sql
+++ index.sql
@@ -3,21 +3,5 @@ select 'shell' as component,
        'custom_theme' as theme,
        'Inter' as font,
        '[NASA International Space Apps Challenge 2025](https://github.com/E11aW/nasa-hackathon)' as footer;
-
-/* select
-    'html' as component,
-        '<h1>Sustainable City Planning</h1>' as html;
-
-SELECT 
-    'html' as component,
-    '<style>img { width: auto; height: auto; border: 4px solid pink; }</style> <img src="NASA_Hackathon_Logo.png" alt="A purple cityscape with trees with the words Sustainable City Planning underneath.">'
- */
-
+ 
 SELECT 'map' AS component;
-
-/* SELECT 'html' as component,
-        '<body>
-            <h1>Header</h1>
-            <p>Lorem ipsum dolor sit amet</p>
-            <p>Lorem ipsum dolor sit amet</p>
-        </body>' as html; */
