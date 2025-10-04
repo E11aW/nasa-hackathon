f6514338b79cd9b3ff440ac40636dfd8c1dc9192
AbbyHoyt <abby.hoyt@comcast.net>
2025-10-04 12:05:09 -0700

    Added multiple HTML elements in one query.

--- index.sql
+++ index.sql
@@ -3,3 +3,16 @@ SELECT 'map' AS component;
 select
     'html' as component,
     '<style>h1 {color: red;}</style> <h1>HTML snippet that is safe because it is hardcoded.</h1>' as html;
+
+select
+    'html' as component,
+    '<style>p {color: black;}</style> <p>Lorem ipsum dolor sit amet</p>' as html;
+
+select
+    'html' as component,
+    '<style>p {color: black;}</style> 
+        <body>
+            <h1>Header</h1>
+            <p>Lorem ipsum dolor sit amet</p>
+            <p>Lorem ipsum dolor sit amet</p>
+        </body>' as html;
